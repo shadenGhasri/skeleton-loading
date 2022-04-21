6cda478f1449f8f8bdf7d560fd5473480e83a4cc
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-04-21 18:30:52 +0430

    add

--- users/src/components/users.jsx
+++ users/src/components/users.jsx
@@ -6,8 +6,8 @@ class Users extends Component {
   };
   render() {
     return (
-      <>
-        <button type="button" className="btn btn-primary">
+      <div className="border border-light">
+        <button type="button" className="btn btn-success m-5">
           Create
         </button>
         <div className="row">
@@ -23,11 +23,19 @@ class Users extends Component {
                   {user.first_name} {user.last_name}
                 </h4>
                 <h5>{user.email}</h5>
+                <div className="row">
+                    <div className="col-6">
+                        <button type="button" class="btn btn-primary" onClick={this.handleUpdate}>Update</button>
+                    </div>
+                    <div className="col-6">
+                    <button type="button" class="btn btn-danger" onClick={this.handleDelete}>Delete</button>
+                    </div>
+                </div>
               </div>
             );
           })}
         </div>
-      </>
+      </div>
     );
   }
 }
--- users/src/index.js
+++ users/src/index.js
@@ -1,10 +1,5 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import "bootstrap/dist/css/bootstrap.main.css";
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
-ReactDOM.render(
-  <App/>,document.getElementById("root")
-);
-
-
+ReactDOM.render(<App/>, document.getElementById('root'));
