7e1083db2c5fcbe1c0dd87bf6d337d400c879ef6
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-04-21 17:36:58 +0430

    add

---
+++ users/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import "bootstrap/dist/css/bootstrap.main.css"
+import App from './App';
+
+ReactDOM.render(
+  <App/>,document.getElementById("root");
+)
+
+
